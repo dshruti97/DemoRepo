cff417cd6dea977c8f64b9ebd486bd58a69b3932
Shruti Desai <shruti.desai@ibm.com>
2022-06-06 11:02:58 +0530

    Added for loop

--- Abc.java
+++ Abc.java
@@ -1,5 +1,7 @@
 public class Abc {
   public static void main(String args[]) {
-    System.out.println("HI Everybody");
+    for (int i = 0; i <= 10; i++) {
+      System.out.println(i);
+    }
   }
 }
